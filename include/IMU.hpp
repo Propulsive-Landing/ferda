54e5b30287fba34f6b7fcdf90d6680ac8e3614d3
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-10-01 20:46:57 +0000

    Add class and function declarations for IMU

---
+++ include/IMU.hpp
@@ -0,0 +1,10 @@
+#include <tuple>
+
+
+class IMU
+{
+    public:
+        IMU();
+        tuple<double, double, double> GetBodyAngularRate(); // Returns angular rate, p, q, and r in order
+        tuple<double, double, double> GetBodyAcceleration(); // Returns linear acceleration, x, y, z
+};
