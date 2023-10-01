efd4b16c756c0a8ff33e659aae173559feb2b12e
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-10-01 21:35:16 +0000

    Add imu class

---
+++ hardware_test/IMU.cpp
@@ -0,0 +1,10 @@
+#include <tuple>
+
+#include "IMU.hpp"
+
+tuple<double, double, double> IMU::GetBodyAngularRate()
+{
+    tuple<double, double, double> temp = make_tuple(0,0,0);
+
+    return temp;
+}
