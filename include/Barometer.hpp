8d18cabe66cf2a239a73017963c3826ebb31cc11
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-10-03 23:13:04 +0000

    add example main file

--- include/Barometer.hpp
+++ include/Barometer.hpp
@@ -7,9 +7,9 @@ class Barometer
 private:
     static constexpr const char * sPressurePath = "/sys/bus/iio/devices/ms5611/in_voltage0_raw";
     static constexpr const char * sTemperaturePath = "/sys/bus/iio/devices/ms5611/in_temp0_raw";
-
+    
 public:
-    Barometer();
+    Barometer() = default;
 
     double GetPressure();
     double GetTemperature();
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,10 @@
+#include "Barometer.hpp"
+#include "IMU.hpp"
+
 int main()
 {
+    IMU imu;
+    Barometer barometer;
 
+    barometer.GetPressure();
 }
