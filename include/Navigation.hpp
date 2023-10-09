a88a245e1dbb802d7a3764f8b2499e0d4ad236c4
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-10-09 23:40:21 +0000

    Add boilerplate for navigation code

--- include/Navigation.hpp
+++ include/Navigation.hpp
@@ -6,5 +6,6 @@ class Navigation
 {
     public:
         Navigation() = default;
-        Eigen::Matrix<double, 3, 1> GetNav();
+        Eigen::Matrix<double, 3, 1> GetNavigation(); // Defintion of navigation matrix: TODO
+        void UpdateNavigation(Eigen::Matrix<double, 4, 1> updates); // Defintion updates: TODO
 };
