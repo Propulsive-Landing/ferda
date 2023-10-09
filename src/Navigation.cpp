d578cd62ec5eb8180fb99d709cebd068072241a5
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-10-09 19:46:02 -0400

    Layout Navigation class (#12)
    * Add empty
    
    * Add eigen library
    
    * Update temporary function
    
    * Add boilerplate for navigation code
    
    * Add todo coments

---
+++ src/Navigation.cpp
@@ -0,0 +1,20 @@
+#include "Navigation.hpp"
+
+Eigen::Matrix<double, 3, 1> Navigation::GetNavigation()
+{
+    // TODO Return the current navigation state
+
+    Eigen::Matrix<double, 3, 1> m = {      // construct a 2x2 matrix
+      {1},     // first row
+      {3},      // second row
+      {5},      // Third row
+    };
+   
+   return m;
+
+}
+
+void Navigation::UpdateNavigation(Eigen::Matrix<double, 4, 1> updates){
+    // TODO Calculate navigation state (angle, position, etc) from the input 
+    // TODO Update the navigation state 
+}
