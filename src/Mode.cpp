9ca0443fe39c66a04c484662bc60f303f858c5aa
AbhiG7 <43866181+AbhiG7@users.noreply.github.com>
2023-10-22 20:49:53 +0000

    added comments

--- src/Mode.cpp
+++ src/Mode.cpp
@@ -5,24 +5,41 @@
 
 Mode::Mode(Phase eInitialMode) : eCurrentMode(eInitialMode) {}
 
-Mode::Phase Mode::UpdateIdle(){ return Mode::Launch; } // TODO Implement idle state behavior
+Mode::Phase Mode::UpdateIdle() {
+    // TODO: Implement idle state behavior
+    // TODO: calculate next stage
+    return Mode::Launch;
+}
 
 
-Mode::Phase Mode::UpdateLaunch(Navigation navigation, Controller controller, double change_time){
+Mode::Phase Mode::UpdateLaunch(Navigation navigation, Controller controller, double change_time) {
 
     navigation.UpdateNavigation();
     controller.UpdateLaunch(navigation);
 
     // TODO Calculate next stage
+    // check for aborts
+    //  if we're too far slanted, set throttle paddles to max
+    // if acceleration == -9.81, return Mode::Freefall
 
     return Mode::Land;
 }
 
-Mode::Phase Mode::UpdateLand(){ return Mode::Terminate; } // TODO Implement land state behavior
+UpdateFreefall() {
+    // some checks
+    navigation.UpdateNavigation();
+
+    // if angle is too far, abort
+    // x[4]
+    // stateMat = [x, y, z, xdot, ydot, zdot, phi, theta, psi, phidot, thetadot, psidot]
+}
+
+Mode::Phase Mode::UpdateLand() {
+    return Mode::Terminate;
+} // TODO Implement land state behavior
 
 
-bool Mode::Update(Navigation& navigation, Controller& controller)
-{
+bool Mode::Update(Navigation& navigation, Controller& controller) {
     static auto last_time = std::chrono::high_resolution_clock::now();
     auto time_now = std::chrono::high_resolution_clock::now();
     double change_time = (time_now.time_since_epoch() - last_time.time_since_epoch()).count();
