f0728bba737cb751359dd7db507fa58a5f13c3d6
Caleb Heydon <caleb@caleb.engineer>
2023-12-02 21:31:53 +0000

    Fixed mode errors

--- include/Controller.hpp
+++ include/Controller.hpp
@@ -27,6 +27,6 @@ public:
     void UpdateLaunch(Navigation& navigation);
     void UpdateLand();
     void UpdateSafe();
-    void HandleAborts(); // Method to handle any abort that comes in (might need enum of aborts)
+    void HandleAborts(int abort); // Method to handle any abort that comes in (might need enum of aborts)
 
 };
