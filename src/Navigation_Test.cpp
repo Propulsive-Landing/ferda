debc2a092c03225289d07ed60c9aff12c0850987
AbhiG7 <43866181+AbhiG7@users.noreply.github.com>
2024-02-01 06:27:30 +0000

    Added one test

--- src/Navigation_Test.cpp
+++ src/Navigation_Test.cpp
@@ -1,1 +1,18 @@
 #include <gtest/gtest.h>
+#include <Eigen/Dense>
+
+#include "Navigation.hpp"
+
+TEST(NavigationTest, RotationalMatrixHappyPath) {
+    double phi = 3.141592653589793238462643383;
+    double theta = phi/2;
+    double psi = 0;
+
+    Eigen::Matrix3d res = {
+        {0, 0, -1},
+        {0, -1, 0},
+        {-1, 0, 0}
+    };
+
+    ASSERT(Navigation::CreateRotationalMatrix(phi, theta, psi), res);
+}
