ea7571161fa04cc5578fa76090e2a7d44b263d17
jackbogatz <114309871+jackbogatz@users.noreply.github.com>
2024-02-02 16:48:31 +0000

    Added git clone https://github.com/google/googletest.git in gtest.yml in hopes that it googltest will be cloned in every offset branch of unitTestPractice

--- src/Navigation_Test.cpp
+++ src/Navigation_Test.cpp
@@ -3,16 +3,22 @@
 
 #include "Navigation.hpp"
 
+
 TEST(NavigationTest, RotationalMatrixHappyPath) {
     double phi = 3.141592653589793238462643383;
     double theta = phi/2;
     double psi = 0;
+    Barometer b = Barometer();
+    TVC t = TVC();
+    IMU i = IMU();
+    Navigation s = Navigation(i,b,t);
 
-    Eigen::Matrix3d res = {
-        {0, 0, -1},
-        {0, -1, 0},
-        {-1, 0, 0}
-    };
 
-    ASSERT(Navigation::CreateRotationalMatrix(phi, theta, psi), res);
+    Eigen::Matrix3d res;
+      res<<  0, 0, -1,
+            0, -1, 0,
+            -1, 0, 0;
+    
+
+    ASSERT_EQ(s.CreateRotationalMatrix(phi, theta, psi), res);
 }
