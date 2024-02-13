bafa8476a32089a55d1a7e50c30d2cec63588c88
JunaidShah123 <118068628+JunaidShah123@users.noreply.github.com>
2024-02-13 19:01:43 +0000

    Integrating IMU readings to PID

--- src/ReactionWheel.cpp
+++ src/ReactionWheel.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 #include <cmath>
 #include <array>
+#include <memory>
+#include <thread>
+#include "Imu.hxx"
+#include "Uart.hxx"
+
+
+int main() {
+    imuUart = uart
+    imu = std::make_shared<Imu>(imuUart);
+	imuData = std::make_shared<struct Imu::Data>();
+
+	std::thread imuThread{[]()
+	{
+		while (true)
+		{
+			imu->update();
+			*imuData = imu->get();
+
+			std::cout<< mission_time << ": reading_imu\n";
+
+			std::cout << mission_time << ":" << imuData->az << "\n";
+		}
+	}};
 
     double setpoint = 0; // desired output  
-    double processVariable = 0.09; // current output  
     double error; // difference between setpoint and processVariable  
     double previousError = 0.09; // error in previous iteration  
     double integral ; // integral of error  
@@ -12,23 +34,25 @@
     double ki = 0.01; // integral gain  
     double kd = 0.0001; // derivative gain  
     double output; // output of the controller  
-    double fsw_sim_time = 0.005; // time step
+    double fsw_sim_time = 0.005; // time step (dt)
+    double flight_time = 16; // time of entire flight
     double fsw_calibration_time = 10;
     double structures_I_hat_zz = 0.0031768;
     double wheel_omega;
     double wheel_I_hat_zz = 0.00007053;
     
-    int main() {
-        for (double i = 1; i <= 3600; ++i) {
-            double calculateOutput(double setpoint, double processVariable);
-            error = setpoint - processVariable;
-            integral += error * fsw_sim_time;
-            derivative = (error - previousError) / fsw_sim_time;
-            output = kp * error + ki * integral + kd * derivative;
-            previousError = error;
-            processVariable = output;
+    int total_iterations = static_cast<int>(16.0 / fsw_sim_time)
+    for (double i = 1; i <= total_iterations; ++i) {
+        double processVariable = imuData->az; // current output from imu
+        error = setpoint - processVariable;
+        integral += error * fsw_sim_time;
+        derivative = (error - previousError) / fsw_sim_time;
+        output = kp * error + ki * integral + kd * derivative;
+        previousError = error;
+        processVariable = output;
 
-            // Print the result
-            std::cout << "PID: " << error << std::endl;}
+        // Print result
+        std::cout << "PID: " << output << std::endl;}
 
-            return 0;}
+    imuThread.join();
+    return 0;}
