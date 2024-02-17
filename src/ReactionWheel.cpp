6f142c1eb8cc0d42384b29e6e00afe9917c64072
JunaidShah123 <118068628+JunaidShah123@users.noreply.github.com>
2024-02-17 01:45:46 +0000

    Trying to implement IMU with PID but "undefined reference to `pthread_create'"?

--- src/ReactionWheel.cpp
+++ src/ReactionWheel.cpp
@@ -3,56 +3,53 @@
 #include <array>
 #include <memory>
 #include <thread>
-#include "Imu.hxx"
-#include "Uart.hxx"
+#include "IMU.hpp"
+#include <chrono>
 
 
 int main() {
-    imuUart = uart
-    imu = std::make_shared<Imu>(imuUart);
-	imuData = std::make_shared<struct Imu::Data>();
-
-	std::thread imuThread{[]()
+    IMU imu;
+    bool Exitloop = false;
+	std::thread imuThread{[&imu, &Exitloop]()
 	{
-		while (true)
+        while (!Exitloop)
 		{
-			imu->update();
-			*imuData = imu->get();
+			std::tuple<double,double,double> angularRate = imu.GetBodyAngularRate();
+            double yaw = std::get<2>(angularRate); // yaw from imu
+            double setpoint = 0; // desired output  
+            double error; // difference between setpoint and processVariable  
+            double previousError = 0.09; // error in previous iteration  
+            double integral ; // integral of error  
+            double derivative; // derivative of error  
+            double kp = 0.01; // proportional gain  
+            double ki = 0.01; // integral gain  
+            double kd = 0.0001; // derivative gain  
+            double output; // output of the controller  
+            double fsw_sim_time = 0.005; // time step (dt)
+            double flight_time = 16; // time of entire flight
+            double fsw_calibration_time = 10;
+            double structures_I_hat_zz = 0.0031768;
+            double wheel_omega;
+            double wheel_I_hat_zz = 0.00007053;
 
-			std::cout<< mission_time << ": reading_imu\n";
+			std::cout << "Angular Rate: " << yaw << "\n"; 
+            int total_iterations = static_cast<int>(16.0 / fsw_sim_time);
+            for (double i = 1; i <= total_iterations; ++i) {
+                double processVariable = yaw; // current output from imu
+                error = setpoint - processVariable;
+                integral += error * fsw_sim_time;
+                derivative = (error - previousError) / fsw_sim_time;
+                output = kp * error + ki * integral + kd * derivative;
+                previousError = error;
+                processVariable = output;
 
-			std::cout << mission_time << ":" << imuData->az << "\n";
+                std::cout << "PID: " << output << std::endl;}// Print result
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));  //delay
 		}
 	}};
 
-    double setpoint = 0; // desired output  
-    double error; // difference between setpoint and processVariable  
-    double previousError = 0.09; // error in previous iteration  
-    double integral ; // integral of error  
-    double derivative; // derivative of error  
-    double kp = 0.01; // proportional gain  
-    double ki = 0.01; // integral gain  
-    double kd = 0.0001; // derivative gain  
-    double output; // output of the controller  
-    double fsw_sim_time = 0.005; // time step (dt)
-    double flight_time = 16; // time of entire flight
-    double fsw_calibration_time = 10;
-    double structures_I_hat_zz = 0.0031768;
-    double wheel_omega;
-    double wheel_I_hat_zz = 0.00007053;
-    
-    int total_iterations = static_cast<int>(16.0 / fsw_sim_time)
-    for (double i = 1; i <= total_iterations; ++i) {
-        double processVariable = imuData->az; // current output from imu
-        error = setpoint - processVariable;
-        integral += error * fsw_sim_time;
-        derivative = (error - previousError) / fsw_sim_time;
-        output = kp * error + ki * integral + kd * derivative;
-        previousError = error;
-        processVariable = output;
-
-        // Print result
-        std::cout << "PID: " << output << std::endl;}
+    std::this_thread::sleep_for(std::chrono::seconds(16));
+    Exitloop = true;
 
     imuThread.join();
     return 0;}
