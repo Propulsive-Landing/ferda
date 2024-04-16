b09900cb77304317d4219e821a24410c00ca943e
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2024-04-16 22:42:33 +0000

    Update test version

--- hardware_test/TVC.cpp
+++ hardware_test/TVC.cpp
@@ -3,39 +3,26 @@
 #include <iostream>
 #include <string>
 
+
 void TVC::SetXServo(double dAngle)
 {
-    /* trim */
-    double commanded = dAngle;
-    if (abs(commanded) > MissionConstants::kMaximumTvcAngleDeg)
-    {
-        commanded *= MissionConstants::kMaximumTvcAngleDeg / abs(commanded);
-    }
+    dAngle += 90 + MissionConstants::kTvcYCenterAngle;
+    dAngle = (dAngle < 0) ? 0 : dAngle;
+    dAngle = (dAngle > 180) ? 180 : dAngle;
 
-
-
-    Eigen::Matrix<double, 2, 1> input_vector = (Eigen::MatrixXd(2,1) << 
-                        dAngle, 
-                        0.0).finished();
-
-
-
-    Eigen::Matrix<double, 2, 1> u_servo = TVC_GEAR_RATIO * input_vector + TVC_SERVO_HORN_OFFSET;
-    double servo_x = u_servo(0, 0);
-    
-
-    double inputAngle = servo_x;
-    inputAngle = (inputAngle < 0) ? 0 : inputAngle;
-    inputAngle = (inputAngle > 180) ? 180 : inputAngle;
-
-    double dPulseWidth = 1000 + (inputAngle * 1000 / 180.0);
-
-    std::cout << "Input angle: " << std::to_string(dAngle) << " Wrote angle: " << std::to_string(inputAngle) << " Width:" << std::to_string(dPulseWidth) << " to x servo\n";
-    return;
+    double dPulseWidth = 1000 + (dAngle * 1000 / 180.0);
+    std::cout << "Wrote angle to X: " + std::to_string(dAngle) + " PW: " + std::to_string(dPulseWidth);
 }
+
+
 
 void TVC::SetYServo(double dAngle)
 {
-    std::cout << "Wrote: " << std::to_string(dAngle) << " to y servo\n";
-    return;
+    dAngle += 90 + MissionConstants::kTvcYCenterAngle;
+    dAngle = (dAngle < 0) ? 0 : dAngle;
+    dAngle = (dAngle > 180) ? 180 : dAngle;
+
+    double dPulseWidth = 1000 + (dAngle * 1000 / 180.0);
+
+    std::cout << "Wrote angle to Y: " + std::to_string(dAngle) + " PW: " + std::to_string(dPulseWidth);
 }
