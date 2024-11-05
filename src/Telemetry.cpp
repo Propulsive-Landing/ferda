01b3281590e9b08f295458fb566fbeb65b89a2e2
gordonbchen <gordonbchen@gmail.com>
2024-11-05 18:11:34 -0500

    send json

--- src/Telemetry.cpp
+++ src/Telemetry.cpp
@@ -7,6 +7,9 @@
 #include <sys/poll.h> 
 #include <fstream>
 
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
 #include "Mode.hpp"
 #include "RF.hpp"
 #include "Telemetry.hpp"
@@ -47,18 +50,18 @@ void Telemetry::Log(std::string message) {
     auto time_now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(time_now);
 
+    json json_msg;
+    json_msg["data_type"] = "string";
+    json_msg["payload"] = message;
 
-
-
-    RF::GetInstance().SendString(message);
+    RF::GetInstance().SendString(json_msg.dump());
 
     std::cout << message << "\n";
 
-    Logs << std::put_time(std::localtime(&in_time_t), "%c") << ",";
-
     // Write data to file
+    Logs << std::put_time(std::localtime(&in_time_t), "%c") << ",";
     Logs << message << "\n" << std::flush;
-}
+} 
 
 void Telemetry::RfSendFrame(Navigation& navigation, Controller& controller)
 {
@@ -66,20 +69,38 @@ void Telemetry::RfSendFrame(Navigation& navigation, Controller& controller)
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
-    
-    frame.mode = 0;
-    frame.euler[0] = 1.0;// navigation.GetNavigation()(0, 0);
-    frame.euler[1] = 2.0; // navigation.GetNavigation()(1, 0);
-    frame.euler[2] = 3.0; // navigation.GetNavigation()(2, 0);
-    frame.input[0] = 5.0; // controller.input(0);
-    frame.input[1] = 6.0; // controller.input(1);
-    frame.velocity[0] = 7.0; // navigation.GetNavigation()(3, 0);
-    frame.velocity[1] = 8.0; // navigation.GetNavigation()(4, 0);
-    frame.velocity[2] = 9.0; // navigation.GetNavigation()(5, 0);
-    frame.dt = 0.0;
-    
-    RF::GetInstance().SendFrame(frame);
+    json json_msg;
+    json_msg["data_type"] = "telem";
+    json_msg["payload"] = {
+        // Euler.
+        navigation.GetNavigation()(0, 0),
+        navigation.GetNavigation()(1, 0),
+        navigation.GetNavigation()(2, 0),
 
+        // Input.
+        controller.input(0),
+        controller.input(1),
+
+        // Velocity.
+        navigation.GetNavigation()(3, 0),
+        navigation.GetNavigation()(4, 0),
+        navigation.GetNavigation()(5, 0),
+
+        // dt.
+        0.0,
+    };
+    
+    // frame.euler[0] = 1.0;// navigation.GetNavigation()(0, 0);
+    // frame.euler[1] = 2.0; // navigation.GetNavigation()(1, 0);
+    // frame.euler[2] = 3.0; // navigation.GetNavigation()(2, 0);
+    // frame.input[0] = 5.0; // controller.input(0);
+    // frame.input[1] = 6.0; // controller.input(1);
+    // frame.velocity[0] = 7.0; // navigation.GetNavigation()(3, 0);
+    // frame.velocity[1] = 8.0; // navigation.GetNavigation()(4, 0);
+    // frame.velocity[2] = 9.0; // navigation.GetNavigation()(5, 0);
+    // frame.dt = 0.0;
+    
+    RF::GetInstance().SendString(json_msg.dump());
 }
 
 
