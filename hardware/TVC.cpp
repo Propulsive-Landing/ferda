03633f2be8c47ddb2c5e07d8b3fb769f5be312d4
gordonbchen <gordonbchen@gmail.com>
2025-03-28 17:21:22 -0400

    clamp input angle rad instead of converted degrees

--- hardware/TVC.cpp
+++ hardware/TVC.cpp
@@ -11,8 +11,8 @@
 
 void TVC::SetTVCX(double angle_rad)
 {
+    angle_rad = std::clamp(angle_rad, -10.0 * MissionConstants.kDeg2Rad, 10.0 * MissionConstants.kDeg2Rad);
     double degrees = (MissionConstants::kTvcXInputCenterAngleRad + angle_rad) * MissionConstants::kRad2Deg;
-    degees = std::clamp(degrees, -10.0, 10.0);  // Clamp to stay in linearized range.
 
     double servoAngle = -.000095801*powf(degrees, 4) - .0027781*powf(degrees, 3) + .0012874*powf(degrees, 2) - 3.1271*degrees -16.129;
     servoAngle += 90 + MissionConstants::kTvcXCenterAngleDeg;
@@ -23,8 +23,8 @@ void TVC::SetTVCX(double angle_rad)
 
 void TVC::SetTVCY(double angle_rad)
 {
+    angle_rad = std::clamp(angle_rad, -10.0 * MissionConstants.kDeg2Rad, 10.0 * MissionConstants.kDeg2Rad);
     double degrees = (MissionConstants::kTvcYInputCenterAngleRad + angle_rad) * MissionConstants::kRad2Deg;
-    degees = std::clamp(degrees, -10.0, 10.0);
     
     double servoAngle = - .0002314576*powf(degrees, 4) - .002425139*powf(degrees, 3) - .01204116*powf(degrees, 2) - 2.959760*degrees + 57.18794;
     servoAngle += 90 + MissionConstants::kTvcYCenterAngleDeg;
