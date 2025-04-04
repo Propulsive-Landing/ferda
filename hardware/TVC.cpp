85579c4478369e274898f0363b96b0badc11b237
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2025-04-04 17:04:17 -0400

    Feature workflow improvements (#45)
    * Update workflow
    
    * Update comment, null input to debug
    
    * Safety check before reading
    
    * Install pigpio from source
    
    * Fix import
    
    * Update typo
    
    * Update c++ version

--- hardware/TVC.cpp
+++ hardware/TVC.cpp
@@ -4,14 +4,14 @@
 
 #include <pigpio.h>
 #include <math.h>
-#include <algorithm.h>
+#include <algorithm>
 #include <MissionConstants.hpp>
 #include <iostream>
 #include <string>
 
 void TVC::SetTVCX(double angle_rad)
 {
-    angle_rad = std::clamp(angle_rad, -10.0 * MissionConstants.kDeg2Rad, 10.0 * MissionConstants.kDeg2Rad);
+    angle_rad = std::clamp(angle_rad, -10.0 * MissionConstants::kDeg2Rad, 10.0 * MissionConstants::kDeg2Rad);
     double degrees = (MissionConstants::kTvcXInputCenterAngleRad + angle_rad) * MissionConstants::kRad2Deg;
 
     double servoAngle = -.000095801*powf(degrees, 4) - .0027781*powf(degrees, 3) + .0012874*powf(degrees, 2) - 3.1271*degrees -16.129;
@@ -23,7 +23,7 @@ void TVC::SetTVCX(double angle_rad)
 
 void TVC::SetTVCY(double angle_rad)
 {
-    angle_rad = std::clamp(angle_rad, -10.0 * MissionConstants.kDeg2Rad, 10.0 * MissionConstants.kDeg2Rad);
+    angle_rad = std::clamp(angle_rad, -10.0 * MissionConstants::kDeg2Rad, 10.0 * MissionConstants::kDeg2Rad);
     double degrees = (MissionConstants::kTvcYInputCenterAngleRad + angle_rad) * MissionConstants::kRad2Deg;
     
     double servoAngle = - .0002314576*powf(degrees, 4) - .002425139*powf(degrees, 3) - .01204116*powf(degrees, 2) - 2.959760*degrees + 57.18794;
--- hardware_test/RF.cpp
+++ hardware_test/RF.cpp
@@ -35,7 +35,7 @@ void RF::SendString(std::string message)
 
 
 
-RF::Command RF::GetCommand() // Will check for commands and return the received command. Non-blocking.
+RF::Command RF::GetCommand() // Will check for commands and return the received command. Non-blocking. Called frequently
 {
     struct pollfd fds;
     int ret;
@@ -46,8 +46,13 @@ RF::Command RF::GetCommand() // Will check for commands and return the received 
     if(ret != 1) // Return if no data
         return RF::Command::None;
 
+    // Extra safety check before reading
+    if (std::cin.eof() || !std::cin.good())
+        return RF::Command::None;
+
     std::string input_line;
     std::getline(std::cin, input_line);
+
     std::cout << "GOT: " << input_line << "\n" << std::flush;
 
     return ParseCommand(input_line);
