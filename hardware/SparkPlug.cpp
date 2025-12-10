3f5a97f59ed350516a3f545b5900f506ecee7a24
Benjamin Deutsch <benjaminrdeutsch@gmail.com>
2025-12-10 16:33:20 -0500

    Implemented SparkPlug for ignition control

---
+++ hardware/SparkPlug.cpp
@@ -0,0 +1,24 @@
+#include "SparkPlug.hpp"
+#include <pigpio.h>
+
+void SparkPlug::TurnOn()
+{
+    gpioWrite(MissionConstants::kSparkPin, 0); // LOW = ON (relay control)
+    // Set PWM to 2% duty cycle (5/255 for 8-bit PWM, but using 0-100 range)
+    // For pigpio, we need to use gpioPWM which takes 0-255, so 2% = ~5
+    gpioPWM(MissionConstants::kRPMPin, 5); // 2% duty cycle
+    isOn = true;
+}
+
+void SparkPlug::TurnOff()
+{
+    gpioWrite(MissionConstants::kSparkPin, 1); // HIGH = OFF
+    gpioPWM(MissionConstants::kRPMPin, 0);     // 0% duty cycle
+    isOn = false;
+}
+
+bool SparkPlug::IsOn() const
+{
+    return isOn;
+}
+
