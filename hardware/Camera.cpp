f42a688e4f235764a68685701634948db3753a32
Harrison <harrison@DESKTOP-PFO1CTL.localdomain>
2026-03-03 22:51:29 -0500

    Placeholder Hardware Files

---
+++ hardware/Camera.cpp
@@ -0,0 +1,15 @@
+// TODO: Update camera class for hardware implementaiton
+
+// hardware_simulation/Camera.cpp
+#include "Camera.hpp"
+
+Camera::Camera() {}
+
+std::tuple<double, double, double, double, double, double, double, double, double> Camera::GetUnitVectors()
+{
+    return std::make_tuple(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+}
+std::tuple<double> Camera::CameraAvailable()
+{
+    return 0.0;
+}
---
+++ hardware/GPS.cpp
@@ -0,0 +1,16 @@
+// Update GPS class for hardware implementation
+
+// hardware_simulation/GPS.cpp
+#include "GPS.hpp"
+
+GPS::GPS() {}
+
+std::tuple<double, double, double> GPS::GetGPSPosition()
+{
+    return std::make_tuple(0.0, 0.0, 0.0);
+}
+
+std::tuple<double> GPS::GPSAvailable()
+{
+    return 0.0;
+}
