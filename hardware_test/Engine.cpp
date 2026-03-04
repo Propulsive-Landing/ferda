3ec274af27d8b3a1b8aa007557dcde2a104b9d2c
Harrison <harrison@DESKTOP-PFO1CTL.localdomain>
2026-03-03 23:14:03 -0500

    Removed Barometer

--- hardware_test/Engine.cpp
+++ hardware_test/Engine.cpp
@@ -1,6 +1,6 @@
 // Update Engine class for hardware implementation
 
-// hardware_simulation/Engine.cpp
+// hardware_test/Engine.cpp
 #include "Engine.hpp"
 
 void Engine::SetThrust(double thrust_N) {
