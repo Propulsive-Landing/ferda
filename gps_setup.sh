eec665146ae8487b09be0c4894f100d92863d4f8
Uconn Rocketry <jack.bogatz@uconn.edu>
2026-04-05 17:56:14 -0400

    Used pythonscript to get fix

--- gps_setup.sh
+++ gps_setup.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-PORT = "/dev/ttyUSB0"
+PORT="/dev/ttyUSB0"
+
+echo "Activating virtual environment..."
+source /home/pi/Documents/venv/bin/activate
+
+echo "Waiting until GPS is fixed" 
+python gps_startup.py 
+echo "GPS is fix"
 
 # Configure terminal settings to be raw
-stty -f "$PORT" 9600 raw -echo -ixon
-sleep 2  # let GPS boot
+stty -F "$PORT" 9600 raw -echo -ixon
+sleep 3  # let GPS boot
 
 printf '$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n' > "$PORT"
-echo Sent NMEA sentence command
+echo "Sent NMEA sentence command"
 sleep 2  # wait
 
 printf '$PMTK251,38400*27\r\n' > "$PORT"
-echo Sent Baud Rate change command
+echo "Sent Baud Rate change command"
 sleep 2  # wait
 
-stty -f "$PORT" 38400 raw -echo -ixon
+stty -F "$PORT" 38400 raw -echo -ixon
 sleep 2
 
 printf '$PMTK220,100*2F\r\n' > "$PORT"
-echo Sent Update Rate change command
+echo "Sent Update Rate change command"
 
---
+++ gps_startup.py
@@ -0,0 +1,17 @@
+import time
+import adafruit_gps
+import serial
+uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=10)
+
+# Create a GPS module instance.
+gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
+
+# Main loop runs forever printing the location, etc. every second.
+last_print = time.monotonic()
+while True:
+    gps.update()
+    if not gps.has_fix:
+        print("Waiting for fix...")
+        continue
+    print("has fix")
+    break
