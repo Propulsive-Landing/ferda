dfc1893f01b64b8816f912a98ef2547fac12bfbd
Jack Bogatz <jack.bogatz@uconn.edu>
2026-04-04 21:08:38 -0400

    fix

--- gps_setup.sh
+++ gps_setup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-#Configure terminal settings to be raw
-stty -f /dev/cu.usbserial-110 9600 raw -echo -ixon
 
+PORT = "/dev/cu.usbserial-110"
+
+# Configure terminal settings to be raw
+stty -f "$PORT" 9600 raw -echo -ixon
+
+# In the background use cat to open up the termina
 cat /dev/cu.usbserial-110 &
 sleep 2  # let GPS boot
-printf '$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n' > /dev/cu.usbserial-110
+
+printf '$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n' > "$PORT"
 echo Sent NMEA sentence command
 sleep 2  # wait
-printf '$PMTK251,38400*27\r\n' > /dev/cu.usbserial-110
+
+printf '$PMTK251,38400*27\r\n' > "$PORT"
 echo Sent Baud Rate change command
 sleep 2  # wait
-stty -f /dev/cu.usbserial-110 38400 raw -echo -ixon
+
+stty -f "$PORT" 38400 raw -echo -ixon
 sleep 2
-printf '$PMTK220,100*2F\r\n' > /dev/cu.usbserial-110
+
+printf '$PMTK220,100*2F\r\n' > "$PORT"
 echo Sent Update Rate change command
 
-pkill -f "cat /dev/cu.usbserial-110"
+pkill -f "cat "$PORT"
