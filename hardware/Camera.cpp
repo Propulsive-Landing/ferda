c2a168271bd59c9bf6318c3f80f6a1dfc16318f3
Harrison <harrison@DESKTOP-PFO1CTL.localdomain>
2026-04-05 22:48:53 -0400

    Maybe?

--- hardware/Camera.cpp
+++ hardware/Camera.cpp
@@ -250,8 +250,8 @@ bool Camera::InitializeVideoStream()
             std::ostringstream pipeline;
             pipeline << "udpsrc port=5000 ! application/x-rtp,media=video,payload=26,clock-rate=90000 ! rtpjpegdepay ! jpegdec ! videoconvert ! video/x-raw,format=BGR ! appsink drop=true max-buffers=1";
 
-            if (gVideoStream.capture.open(pipeline.str(), cv::CAP_GSTREAMER) || 
-                gVideoStream.capture.open("udp://@127.0.0.1:5000", cv::CAP_FFMPEG)) {
+            std::cerr << "Trying FFmpeg UDP..." << std::endl;
+            if (gVideoStream.capture.open("udp://@127.0.0.1:5000", cv::CAP_FFMPEG)) {
                 
                 std::cerr << "Camera opened with UDP stream at " << width << "x" << height << std::endl;
                 gVideoStream.capture.set(cv::CAP_PROP_BUFFERSIZE, 1.0);
