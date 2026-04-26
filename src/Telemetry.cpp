6225377a277bffa0d44b3d3a315cd484eea5a608
Jack Bogatz <jack.bogatz@uconn.edu>
2026-04-26 11:25:18 -0400

    Log

--- src/Telemetry.cpp
+++ src/Telemetry.cpp
@@ -219,7 +219,7 @@ void Telemetry::RfSendGNCFrame(Navigation &navigation, Controller &controller)
 
     };
 
-    std::cout << "GNC Telemetry: " << json_msg.dump().length() << std::endl;
+    std::cout << "GNC Telemetry: " << json_msg["Payload"].size() << std::endl;
     RF::GetInstance().SendString(json_msg.dump() + "\n");
 }
 
