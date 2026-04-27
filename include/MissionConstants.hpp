2b65dc027f3eafc71b3a702f6e7911724c361438
Harrison <harrison@DESKTOP-PFO1CTL.localdomain>
2026-04-27 00:36:23 -0400

    Changed Constants

--- include/MissionConstants.hpp
+++ include/MissionConstants.hpp
@@ -46,9 +46,9 @@ namespace MissionConstants
     const double kNavGyroWhiteNoiseSigma = 0.0025;
     const double kNavAccelBiasRandomWalkSigma = 0.0;
     const double kNavGyroBiasRandomWalkSigma = 0.0;
-    const double kNavCameraAssociationUnassignedPenaltyRad = 0.25;
+    const double kNavCameraAssociationUnassignedPenaltyRad = 0.15;
     const int kNavCameraRecoveryMinDetectedMarkers = 4;
-    const double kNavCameraRecoveryMaxAverageAngularErrorRad = 0.05;
+    const double kNavCameraRecoveryMaxAverageAngularErrorRad = 0.1;
     const double kNavInitialPositionVariance = 1e-5;
     const double kNavInitialVelocityVariance = 1e-6;
     const double kNavInitialAttitudeVariance = 3e-4;
