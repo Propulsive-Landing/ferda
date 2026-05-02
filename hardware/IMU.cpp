fd188b08687bfbbf6ace3325128d6269798de5cd
Hlpred <Harrisonperone@gmail.com>
2026-05-01 20:25:42 -0400

    bias outside of map

--- hardware/IMU.cpp
+++ hardware/IMU.cpp
@@ -133,7 +133,7 @@ std::tuple<double, double, double> IMU::GetBodyAcceleration()
         nAccelX / 100.0f,
         nAccelY / 100.0f,
         nAccelZ / 100.0f);
-    const Eigen::Vector3d accelBody = MapImuSensorToBody(accelSensor - MissionConstants::kSensorImuAccelBiasSensorMps2);
+    const Eigen::Vector3d accelBody = MapImuSensorToBody(accelSensor) - MissionConstants::kSensorImuAccelBiasSensorMps2;
 
     return std::make_tuple(accelBody.x(), accelBody.y(), accelBody.z());
 }
